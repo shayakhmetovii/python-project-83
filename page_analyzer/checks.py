ce66311b3847f72d086f713e99a4d456a178278b
idolon <shayakhmetovii@icloud.com>
2026-05-25 03:09:48 +0300

    правки после автотестов

--- page_analyzer/checks.py
+++ page_analyzer/checks.py
@@ -1,14 +1,21 @@
 from bs4 import BeautifulSoup
 
 
+def truncate_with_ellipsis(text, max_len=255):
+    if len(text) > max_len:
+        return text[:max_len - 3] + '...'
+    return text
+
+
 def get_check(html_doc):
     soup = BeautifulSoup(html_doc, 'html.parser')
     h1 = soup.h1.string if soup.h1 else ''
     title = soup.title.string if soup.title else ''
     if soup.find('meta', {'name': 'description'}):
         description = soup.find('meta', {'name': 'description'})['content']
-        if len(description) > 255:
-            description = description[:254]
     else:
         description = ''
+    h1 = truncate_with_ellipsis(h1)
+    title = truncate_with_ellipsis(title)
+    description = truncate_with_ellipsis(description)
     return h1, title, description
