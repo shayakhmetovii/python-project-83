a43aefb918afb3550059f5e88a6129bdfd99b3fe
idolon <shayakhmetovii@icloud.com>
2026-05-25 08:43:12 +0300

    поправлен truncate_with_ellipsis, get_check

--- page_analyzer/checks.py
+++ page_analyzer/checks.py
@@ -2,6 +2,8 @@ from bs4 import BeautifulSoup
 
 
 def truncate_with_ellipsis(text, max_len=255):
+    if text is None:
+        return ''
     if len(text) > max_len:
         return text[:max_len - 3] + '...'
     return text
@@ -10,9 +12,15 @@ def truncate_with_ellipsis(text, max_len=255):
 def get_check(html_doc):
     soup = BeautifulSoup(html_doc, 'html.parser')
     h1 = soup.h1.string if soup.h1 else ''
+    if h1 is None:
+        h1 = ''
     title = soup.title.string if soup.title else ''
+    if title is None:
+        title = ''
     if soup.find('meta', {'name': 'description'}):
         description = soup.find('meta', {'name': 'description'})['content']
+        if description is None:
+            description = ''
     else:
         description = ''
     h1 = truncate_with_ellipsis(h1)
